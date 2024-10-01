25c26566f087ef5ea5c3644570d25761ac032854
chxdrick <focal.cdw1010@gmail.com>
2024-10-01 12:06:06 -0400

    Add files via upload

---
+++ cis4615a.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac cis4615a.java
+ *  Execution:    java cis4615a
+ *
+ *  Prints 
+ *    "Hello, World - from the class of CIS4615 - Fall 2022 -UCF _Chadrick Williams_".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java cis4615a
+ *  Hello, World - from the class of CIS4615 - Fall 2022 - UCF
+ *  %
+ *
+ ******************************************************************************/
+
+public class cis4615a {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from the class of CIS4615 - Fall 2022 - UCF");
+    }
+
+}
