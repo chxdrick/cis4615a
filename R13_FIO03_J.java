efb55bbbe0bcccc74680fc10ec5136c30f0f771f
chxdrick <focal.cdw1010@gmail.com>
2024-10-04 08:32:32 -0400

    Create R13_FIO03_J.java
    Rule 13 Documentation. Example of non-compliant code.

---
+++ R13_FIO03_J.java
@@ -0,0 +1,24 @@
+class TempFile {
+  public static void main(String[] args) throws IOException{
+    File f = new File("tempnam.tmp");
+    if (f.exists()) {
+      System.out.println("This file already exists");
+      return;
+    }
+ 
+    FileOutputStream fop = null;
+    try {
+      fop = new FileOutputStream(f);
+      String str = "Data";
+      fop.write(str.getBytes());
+    } finally {
+      if (fop != null) {
+        try {
+          fop.close();
+        } catch (IOException x) {
+          // Handle error
+        }
+      }
+    }
+  }
+}
